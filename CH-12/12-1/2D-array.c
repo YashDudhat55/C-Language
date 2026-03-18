da2e7c3aa11e8252e3e7bc6f84ae29c0c018be44
YashDudhat55 <yashrd2009@gmail.com>
2026-03-18 08:28:11 +0530

    2D Array

--- CH-12/12-1/2D-array.c
+++ CH-12/12-1/2D-array.c
@@ -3,12 +3,14 @@
 
 int main(){
     int a[3][3], i, j;
-    pf("Enter the elements of the 2D array:\n");
     for (i = 0; i < 3; i++){
         for (j = 0; j < 3; j++){
+            pf("Enter the elements of the 2D array : ");
             scanf("%d", &a[i][j]);
         }
     }
+
+    pf("\nOutput of the 2D array : ");
 
     for (i = 0; i < 3; i++){
             pf("\n");
---
+++ CH-12/12-1/TwoDArray.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define pf printf
+
+int main()
+{
+
+    int array[3][4] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 1, 2, 3}};
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            pf("%d ", array[i][j]);
+        }
+        pf("\n");
+    }
+
+    return 0;
+}
