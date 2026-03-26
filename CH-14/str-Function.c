08799c567afdb596c4aa1967b6a999982244b01a
YashDudhat55 <yashrd2009@gmail.com>
2026-03-26 09:11:02 +0530

    string

--- CH-14/str-Function.c
+++ CH-14/str-Function.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#define pf printf
 
 main()
 {
@@ -8,45 +9,45 @@ main()
 
     char cpy[20];
 
-    printf("Enter any string 1 : ");
+    pf("Enter any string 1 : ");
     gets(str);
-    printf("Enter any string 2 : ");
+    pf("Enter any string 2 : ");
     gets(str2);
 
     // strcat(str, str2);
 
-    // printf("%s\n", str);
+    // pf("%s\n", str);
 
     strcpy(cpy, str);
 
-    printf("Copy : %s\n", cpy);
+    pf("Copy : %s\n", cpy);
 
     // puts("");
     // puts(str);
 
     // int len = strlen(str);
-    // printf("String Length : %d\n", strlen(str));
+    // pf("String Length : %d\n", strlen(str));
 
     // strupr(str);
 
-    // printf("UPPERCASf : %s\n", str);
+    // pf("UPPERCASf : %s\n", str);
 
     // strlwr(str);
 
-    // printf("lowercase : %s\n", str);
+    // pf("lowercase : %s\n", str);
 
     // strrev(str);
 
-    // printf("Reverse : %s\n", str);
+    // pf("Reverse : %s\n", str);
 
     int cmp = strcmp(str, str2);
 
     if (cmp == 0)
     {
-        printf("Both string are same %d", cmp);
+        pf("Both string are same %d", cmp);
     }
     else
     {
-        printf("Both string not are equale.... %d", cmp);
+        pf("Both string not are equale.... %d", cmp);
     }
 }
